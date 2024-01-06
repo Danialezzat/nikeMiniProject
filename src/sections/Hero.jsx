17752908080f8a42e8e1940cbccd90681c4b3769
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-06 13:03:41 -0800

    bug fixed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,8 +12,8 @@ const Hero = () => {
     <section id='home' className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 '>
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold '>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[1] font-bold '>
+          <span className='  relative z-10 pr-10'>The New Arrival</span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
